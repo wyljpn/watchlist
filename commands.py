ce803d6396d87da4da6c012f8a30afc09cc62a0b
WANG <wyljpn@gmail.com>
2020-08-06 14:15:14 +0900

    Add database support with Flask-SQLAlchemy

---
+++ commands.py
@@ -0,0 +1,15 @@
+import click
+
+from watchlist import app, db
+from watchlist.models import User, Movie
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
